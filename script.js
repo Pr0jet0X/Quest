2db0ac63fcf8ad6fa6405c4c7bdb02ffc8e40231
Pr0jet0X <111866594+Pr0jet0X@users.noreply.github.com>
2024-07-17 11:15:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,33 +19,33 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Ao começar na escola, começam a reparar que você se dá muito bem em todas as matérias e chamam seus pais para uma reunião. O que você começa a fazer?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Começa a finir ser um aluno normal para evitar futuros problemas.",
+                afirmacao: "Em tempos futuros, teve uma infância e adolescencia tranquila."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Resolve assumir o papel de Gênio Mirim.",
+                afirmacao: "Em pouco tempo, começam a pereber que você tem algo de diferente, então você passa sua infância e adolescência sob muita pressão."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Anos depois, você começa a acertar previsões sobre coisas que acontecerão no futuro. Como vê isso?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Uma ótima oportunidade para ficar famoso e rico.",
+                afirmacao: "No fim de sua adolescência, você entra na mídia com previsões do futuro, mas em poucos anos, você começa a ficar sem previsões e é considerado uma fraude."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Melhor ir guardando isso para mim.",
+                afirmacao: "No fim de sua adolescência, você resolve começar a trabalhar para viver uma vida normal."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Tempos depois,",
         alternativas: [
             {
                 texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
